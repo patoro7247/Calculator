2ea87507cbf0fac2ff8ba40644a8678ecbc50faa
Patrick Harding <patrickhardingr@gmail.com>
2021-08-06 18:41:15 -0700

    First commit

---
+++ calculator.py
@@ -0,0 +1,9 @@
+from tkinter import*
+
+
+# create two variables to store operands
+
+operand1 = input('Enter the first operand')
+operand2 = input('Enter the second operand')
+result = num1 + num2
+print(result)
